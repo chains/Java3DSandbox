9471c75f3742a2c239fb004c1bbf861ef106d3c9
Nyx <simonil89@gmail.com>
2012-03-21 15:12:16 +0100

    Cleaned up the Camera class, added comments

--- Java3DSandbox/src/nyx3DBox/Camera.java
+++ Java3DSandbox/src/nyx3DBox/Camera.java
@@ -14,6 +14,12 @@ import javax.vecmath.Vector3f;
 
 import com.sun.j3d.utils.universe.ViewingPlatform;
 
+
+/**
+ * Camera class. Contains listeners for keys and manipulates the camera accordingly.
+ * @author Nyx
+ *
+ */
 public class Camera implements KeyListener, ActionListener, MouseMotionListener {
 
 	
@@ -23,98 +29,134 @@ public class Camera implements KeyListener, ActionListener, MouseMotionListener 
 	private int mouseY = 0;
 	private int mouseX = 0;
 	
+	
+	/**
+	 * Creates a camera controller for the provided ViewingPlatform
+	 * @param vp
+	 */
 	public Camera(ViewingPlatform vp){
         view_tg = vp.getMultiTransformGroup().getTransformGroup(0);
         view_tf3d = new Transform3D();
         view_tg.getTransform(view_tf3d);
-        
-        view_tg.setTransform(view_tf3d); 
+	}
+	
+	/**
+	 * Sets the camera to the X-axis, looking along it in the positive direction.
+	 */
+	public void setToX(){
+		//TODO
+	}
+	
+	/**
+	 * Sets the camera to the X-axis, looking along it in the negative direction.
+	 */
+	public void setToXInverse(){
+		//TODO
+	}
+	
+	/**
+	 * Sets the camera to the Y-axis, looking along it in the positive direction.
+	 */
+	public void setToY(){
+		//TODO
+	}
+	
+	/**
+	 * Sets the camera to the Y-axis, looking along it in the negative direction.
+	 */
+	public void setToYInverse(){
+		//TODO
+	}
+	
+	/**
+	 * Sets the camera to the Z-axis, looking along it in the positive direction.
+	 */
+	public void setToZ(){
+		//TODO
 	}
 	
 	
+	/**
+	 * Sets the camera to the Z-axis, looking along it in the positive direction.
+	 */
+	public void setToZInverse(){
+		//TODO
+	}
+	
+	/**
+	 * Sets the camera to Free-look mode.
+	 */
+	public void setToFreeLook(){
+		
+	}
 	
 	
+	/**
+	 * Key listeneres
+	 */
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
+		// Unused
 		
 	}
 
+
 	@Override
 	public void keyTyped(KeyEvent e) {
-    	System.out.println("Hai");
     	if(e.getKeyChar() == 'd'){
-    		System.out.println("Bai");
     		Transform3D dirTransf = new Transform3D();
     		dirTransf.setTranslation(new Vector3d(0.05d, 0.0d, 0.0d));
-    		//controlVec.setX((float)(controlVec.getX() + 0.05));
             view_tg.getTransform(view_tf3d);
             view_tf3d.mul(dirTransf);
             view_tg.setTransform(view_tf3d); 
     	}
     	if(e.getKeyChar() == 'a'){
-    		System.out.println("Bai");
     		Transform3D dirTransf = new Transform3D();
     		dirTransf.setTranslation(new Vector3d(-0.05d, 0.0d, 0.0d));
-    		//controlVec.setX((float)(controlVec.getX() - 0.05));
             view_tg.getTransform(view_tf3d);
             view_tf3d.mul(dirTransf);
             view_tg.setTransform(view_tf3d); 
     	}
     	if(e.getKeyChar() == 'w'){
-    		System.out.println("Bai");
     		Transform3D dirTransf = new Transform3D();
     		dirTransf.setTranslation(new Vector3d(0.0d, 0.0d, -0.05d));
-    		//controlVec.setX((float)(controlVec.getX() - 0.05));
             view_tg.getTransform(view_tf3d);
             view_tf3d.mul(dirTransf);
             view_tg.setTransform(view_tf3d); 
     	}
     	if(e.getKeyChar() == 's'){
-    		System.out.println("Bai");
     		Transform3D dirTransf = new Transform3D();
     		dirTransf.setTranslation(new Vector3d(0.0d, 0.0d, 0.05d));
-    		//controlVec.setX((float)(controlVec.getX() - 0.05));
             view_tg.getTransform(view_tf3d);
             view_tf3d.mul(dirTransf);
             view_tg.setTransform(view_tf3d); 
     	}
     	if(e.getKeyChar() == 'q'){
-    		System.out.println("Bai");
     		Transform3D dirTransf = new Transform3D();
     		dirTransf.setTranslation(new Vector3d(0.0d, 0.0d, 0.05d));
-    		//controlVec.setX((float)(controlVec.getX() - 0.05));
             view_tg.getTransform(view_tf3d);
             view_tf3d.mul(dirTransf);
             view_tg.setTransform(view_tf3d); 
     	}
     	if(e.getKeyChar() == 'e'){
-    		System.out.println("Bai");
     		Transform3D dirTransf = new Transform3D();
     		dirTransf.setTranslation(new Vector3d(0.0d, 0.0d, -0.05d));
-    		//controlVec.setX((float)(controlVec.getX() - 0.05));
             view_tg.getTransform(view_tf3d);
             view_tf3d.mul(dirTransf);
             view_tg.setTransform(view_tf3d); 
     	}
     	if(e.getKeyChar() == 'z'){
-    		System.out.println("Bai");
             view_tg.getTransform(view_tf3d);
-            //view_tf3d.setTranslation(new Vector3f(0,0,0));
             Transform3D rotTransf = new Transform3D();
             rotTransf.rotY(0.1);
             view_tf3d.mul(rotTransf);
-            //view_tf3d.setTranslation(controlVec);
             view_tg.setTransform(view_tf3d); 
     	}
     	if(e.getKeyChar() == 'x'){
-    		System.out.println("Bai");
             view_tg.getTransform(view_tf3d);
-            //view_tf3d.setTranslation(new Vector3f(0,0,0));
             Transform3D rotTransf = new Transform3D();
             rotTransf.rotY(-0.1);
             view_tf3d.mul(rotTransf);
-            //view_tf3d.setTranslation(controlVec);
             view_tg.setTransform(view_tf3d); 
     	}
 		
@@ -122,56 +164,49 @@ public class Camera implements KeyListener, ActionListener, MouseMotionListener 
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
+		// Unused
 		
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		System.out.println("Hai");
+		// Unused
 		
 	}
 
 
-
-
+	/**
+	 * Mouse listeners
+	 */
 	@Override
 	public void mouseDragged(MouseEvent e) {
 		System.out.println("Hai");
 		if(e.getY() < mouseY){
             view_tg.getTransform(view_tf3d);
-            //view_tf3d.setTranslation(new Vector3f(0,0,0));
             Transform3D rotTransf = new Transform3D();
             rotTransf.rotX(-0.01);
             view_tf3d.mul(rotTransf);
-            //view_tf3d.setTranslation(controlVec);
             view_tg.setTransform(view_tf3d); 
 		}
 		if(e.getY() > mouseY){
             view_tg.getTransform(view_tf3d);
-            //view_tf3d.setTranslation(new Vector3f(0,0,0));
             Transform3D rotTransf = new Transform3D();
             rotTransf.rotX(0.01);
             view_tf3d.mul(rotTransf);
-            //view_tf3d.setTranslation(controlVec);
             view_tg.setTransform(view_tf3d); 
 		}
 		if(e.getX() > mouseX){
             view_tg.getTransform(view_tf3d);
-            //view_tf3d.setTranslation(new Vector3f(0,0,0));
             Transform3D rotTransf = new Transform3D();
             rotTransf.rotY(0.01);
             view_tf3d.mul(rotTransf);
-            //view_tf3d.setTranslation(controlVec);
             view_tg.setTransform(view_tf3d); 
 		}
 		if(e.getX() < mouseX){
             view_tg.getTransform(view_tf3d);
-            //view_tf3d.setTranslation(new Vector3f(0,0,0));
             Transform3D rotTransf = new Transform3D();
             rotTransf.rotY(-0.01);
             view_tf3d.mul(rotTransf);
-            //view_tf3d.setTranslation(controlVec);
             view_tg.setTransform(view_tf3d); 
 		}
 		mouseX = e.getX();
@@ -180,11 +215,9 @@ public class Camera implements KeyListener, ActionListener, MouseMotionListener 
 	}
 
 
-
-
 	@Override
 	public void mouseMoved(MouseEvent e) {
-		System.out.println("Hai");
+		// Unused
 		
 	}
 	
