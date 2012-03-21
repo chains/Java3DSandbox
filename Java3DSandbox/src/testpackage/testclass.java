5fd50101df8e4f30cb983e62c8fa967a8587fce2
chains <dixtre@gmail.com>
2012-03-21 11:00:09 +0100

    First commit

---
+++ Java3DSandbox/src/testpackage/testclass.java
@@ -0,0 +1,14 @@
+package testpackage;
+
+public class testclass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hej");
+
+	}
+
+}
